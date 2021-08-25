2d7ebce6c36ad917bf19ee2341d2933318b14086
Vika <55987848+samuse-va@users.noreply.github.com>
2021-08-25 09:55:53 +0300

    загружает файлы сайта

---
+++ script.js
@@ -0,0 +1,5 @@
+let offset = 0;
+const sliderLine = document.querySelector('.slider-line');
+let buttonNext = document.querySelector('.slider-next');
+let buttonPrev = document.querySelector('.slider-prev');
+
