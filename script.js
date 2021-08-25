1f09d28fabc61776d4a8355249fd9ae3ced985fc
Vika <55987848+samuse-va@users.noreply.github.com>
2021-08-25 10:12:02 +0300

    добавляет кнопки

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ const sliderLine = document.querySelector('.slider-line');
 let buttonNext = document.querySelector('.slider-next');
 let buttonPrev = document.querySelector('.slider-prev');
 
+buttonNext.addEventListener('click', function() {
+    offset += 256;
+    if (offset > 636) {
+      //buttonNext.disabled = true;
+      offset = 758;
+    //  buttonNext.classList.add('hidden');
+    }
+    sliderLine.style.left = -offset + 'px';
+  
+  });
+  
+  buttonPrev.addEventListener('click', function() {
+    offset -= 256;
+    if (offset < 128) {
+      offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
+  });
